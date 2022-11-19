574709359674532dad1f0d1cf878f551eb5d5147
jevk <jevk@users.noreply.github.com>
2022-11-20 01:23:00 +0200

    YUP

--- script.ts
+++ script.ts
@@ -1,4 +1,4 @@
-import { AnimateTrack, Cinema, CustomData, ease, filter, Map, notes, PathAnimation, PlayerTrack, random, Requirement, Settings, Suggestion, track, TrackParent, Wall } from "./src/main.ts";
+import { Map } from "./src/main.ts";
 const INPUT = 'temp.dat'
 const OUTPUT = 'ExpertPlusLawless.dat'
 
@@ -6,17 +6,8 @@ const difficulty = Map.initialize(INPUT, OUTPUT, 16, 0)
 
 // #region Noodle stuff below
 
-track(filter(notes, 8, 16), ["deez", "nuts"]);
 
-new Requirement("ExpertPlusLawless.dat")
-    .chroma()
-    .noodle()
-    .cinema()
 
-new Cinema("cum")
-    .author("dees")
-    .push();
-new PathAnimation(8).track("deez").duration(2).pos([0, 0, 0]).color("d").dis([0]).disArr([1]).rot("sdasd")
 
 // #endregion Noodle stuff above
 
