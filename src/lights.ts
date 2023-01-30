89d15d23cd597b4c857cfd3783adca8933dae566
Jevk <58623854+jevk@users.noreply.github.com>
2023-01-30 15:31:49 +0200

    Update lights.ts

---
+++ src/lights.ts
@@ -0,0 +1,19 @@
+export class LightEvent {
+    static readonly Direction = {
+        CCW: 0,
+        CW: 1
+    }
+    static readonly Type = {
+        BackLasers: 0,
+        RingLights: 1,
+        LRotatingLasers: 2,
+        RRotatingLasers: 3,
+        CenterLights: 4,
+        BoostColor: 5,
+        ExtraLeft: 6,
+        ExtraRight: 7,
+        RingSpin: 8,
+        RingZoom: 9
+    }
+    static readonly Value = {}
+}
