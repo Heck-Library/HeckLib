105d6551546164efec2715f8bef178475e9761fe
Jevk <58623854+jevk@users.noreply.github.com>
2023-01-30 15:05:34 +0200

    PointDefinition snippet added

--- script.ts
+++ script.ts
@@ -1,16 +1,16 @@
 import { Map } from "./src/main.ts";
-const INPUT = 'ExpertPlusStandard.dat'
-const OUTPUT = 'ExpertPlusLawless.dat'
+const INPUT = 'ExpertPlusStandard.dat';
+const OUTPUT = 'ExpertPlusLawless.dat';
 
 const difficulty = Map.initialize(INPUT, OUTPUT, {
     njs: 16,
     offset: 0
-})
+});
 // #region Noodle stuff below
 
 
 
 // #endregion Noodle stuff above
 Map.finalize(difficulty, {
-    formatting:true
-})
+    formatting: false
+});
