f8bc42c2140791fd1b24d53f9715315614e75e2b
jevk <jevk@users.noreply.github.com>
2023-02-01 22:19:37 +0200

    Added `dupeNote();`

--- src/general.ts
+++ src/general.ts
@@ -1,6 +1,22 @@
 // deno-lint-ignore-file
 
+import { Note } from "./objects.ts";
+import { NOTE } from "./types.ts";
+
 export const __dirname = new URL('.', import.meta.url).pathname.slice(1).replace(/\//g, "\\").replace(/src\\/g, "");
+
+export function dupeNote(obj: NOTE): NOTE {
+    const o = JSON.parse(JSON.stringify(obj)).json;
+    const n: NOTE = new Note({
+        //Vanilla data
+        time: o.nD.time,
+        type: o.nD.type,
+        x: o.nD.x,
+        y: o.nD.y,
+        direction: o.nD.direction,
+    }, o.cD, o.aD);
+    return n;
+}
 
 /**
  * A function for linear interpolation.
