fb39911700115c7740b630986c023027695e973f
Jevk <58623854+jevk@users.noreply.github.com>
2023-03-01 13:16:52 +0200

    Improved snippets

--- example.ts
+++ example.ts
@@ -1,7 +1,6 @@
 import { Map } from './src/mapHandler';
 import { diffFile } from './consts/difficulty';
 import Difficulty from './consts/difficulty';
-import AnimateTrack from './events/animateTrack';
 
 const INPUT: diffFile = Difficulty.Standard.ExpertPlus;
 const OUTPUT: diffFile = Difficulty.Lawless.ExpertPlus;
@@ -11,11 +10,6 @@ const difficulty = Map.initialize(INPUT, OUTPUT, {
     offset: 0
 });
 // #region Noodle stuff below
-
-new AnimateTrack(0, {
-    track: "sfdfj",
-    duration: 0,
-}).push();
 
 
 
