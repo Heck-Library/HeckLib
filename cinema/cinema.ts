37c3a69f7e0e5a07b9a07ca9288d3fda379f4a5a
Jevk <58623854+jevk@users.noreply.github.com>
2023-03-06 13:15:38 +0200

    Cinema config uses an interface now

--- cinema/cinema.ts
+++ cinema/cinema.ts
@@ -1,56 +1,66 @@
 import { writeFileSync } from "fs";
 import { vec3 } from "../consts/types/vec";
 
+interface ICinemaConfig {
+    videoUrl: string;
+    title?: string;
+    author?: string;
+    videoFile: string;
+    duration?: number;
+    offset?: number;
+    configByMapper?: boolean;
+    environmentName?: string;
+    playbackSpeed?: number;
+    loop?: boolean;
+    endVideoAt?: number;
+    screenPosition?: {
+        x: number;
+        y: number;
+        z: number;
+    };
+    screenRotation?: {
+        x: number;
+        y: number;
+        z: number;
+    };
+    screenHeight?: number;
+    screenCurvature?: number;
+    screenSubsurfaces?: number;
+    allowCustomPlatform?: boolean;
+    disableDefaultModifications?: boolean;
+    forceEnvironmentModifications?: boolean;
+    mergePropGroups?: boolean;
+    transparency?: boolean;
+    bloom?: number;
+    colorCorrection?: {
+        brightness?: number;
+        contrast?: number;
+        saturation?: number;
+        exposure?: number;
+        gamma?: number;
+        hue?: number;
+    };
+    vignette?: {
+        type?: "elliptical" | "rectangular";
+        radius?: number;
+        softness?: number;
+    };
+    additionalScreens?: {
+        position: {
+            x: number;
+            y: number;
+            z: number;
+        };
+        rotation: {
+            x: number;
+            y: number;
+            z: number;
+        };
+    }[];
+}
+
 export default class Cinema {
-    config: {
-        videoUrl: string
-        title?: string
-        author?: string
-        videoFile: string
-        duration?: number
-        offset?: number
-        configByMapper?: boolean
-        environmentName?: string
-        playbackSpeed?: number
-        loop?: boolean
-        endVideoAt?: number
-        screenPosition?: {x:number, y:number, z:number}
-        screenRotation?: {x:number, y:number, z:number}
-        screenHeight?: number
-        screenCurvature?: number
-        screenSubsurfaces?: number
-        allowCustomPlatform?: boolean
-        disableDefaultModifications?: boolean
-        forceEnvironmentModifications?: boolean
-        mergePropGroups?: boolean
-        transparency?: boolean
-        bloom?: number
-        colorCorrection?: {
-            brightness?: number
-            contrast?: number
-            saturation?: number
-            exposure?: number
-            gamma?: number
-            hue?: number
-        }
-        vignette?: {
-            type?: "elliptical" | "rectangular"
-            radius?: number
-            softness?: number
-        }
-        additionalScreens?: {
-            position: {
-                x: number
-                y: number
-                z: number
-            }
-            rotation: {
-                x: number
-                y: number
-                z: number
-            }
-        }[]
-    }
+    private config: ICinemaConfig
     constructor(URL: string) {
         this.config = {
             videoUrl: URL,
