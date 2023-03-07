7a634f39d6feb33695a7f2cb7b0d325c8f43928a
Jevk <58623854+jevk@users.noreply.github.com>
2023-03-07 09:56:25 +0200

    PointDefinitions directory moved

--- objects/mod.ts
+++ objects/mod.ts
@@ -1,11 +1,9 @@
 import Note from './note';
-import PointDefinition from './pointDefinition';
 import Wall from './wall';
 import Light from './lights';
 
 const object = {
     Note,
-    PointDefinition,
     Wall,
     Light
 }
