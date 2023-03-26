1a0ff71b9d4a2b2242d7adfe8dec477fa96610de
jevk <jevk@users.noreply.github.com>
2023-03-27 00:53:00 +0300

    Update dupe.ts... again...

--- functions/dupe.ts
+++ functions/dupe.ts
@@ -2,12 +2,13 @@ import { NOTE } from "../consts/types/objects";
 import Note from "../objects/note";
 
 export default function dupe(obj : NOTE): NOTE {
+    const o = JSON.parse(JSON.stringify(obj))
     const n: NOTE = new Note({
-        time: obj.time,
-        type: obj.type,
-        x: obj.x,
-        y: obj.y,
-        direction: obj.direction
-    }, obj.data, obj.anim);
+        time: o.json.nD.time,
+        type: o.json.nD.type,
+        x: o.json.nD.x,
+        y: o.json.nD.y,
+        direction: o.json.nD.direction
+    }, o.json.cD, o.json.aD);
     return n;
 }
