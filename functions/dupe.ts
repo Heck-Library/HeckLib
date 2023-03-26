a704a2ff9491294325fb05d1a26c5dc22a972442
jevk <jevk@users.noreply.github.com>
2023-03-26 02:53:57 +0200

    Update dupe.ts

--- functions/dupe.ts
+++ functions/dupe.ts
@@ -1,30 +1,13 @@
-import { NOTE, WALL } from "../consts/types/objects";
+import { NOTE } from "../consts/types/objects";
 import Note from "../objects/note";
-import Wall from "../objects/wall";
 
-export default function dupe(obj : WALL): WALL
 export default function dupe(obj : NOTE): NOTE {
-    if (obj.direction) {
-        const o: NOTE = JSON.parse(JSON.stringify(obj)).json;
-        const n: NOTE = new Note({
-            // Vanilla data
-            time: o.time,
-            type: o.type,
-            x: o.x,
-            y: o.y,
-            direction: o.direction
-        }, o.data, o.anim);
-        return n;
-    }
-    const o: WALL = JSON.parse(JSON.stringify(obj)).json;
-    const w: WALL = new Wall({
-        //Vanilla data
-        time: o.time,
-        duration: o.duration,
-        height: o.height,
-        width: o.width,
-        x: o.x,
-        y: o.y
-    }, o.data, o.anim);
-    return w;
+    const n: NOTE = new Note({
+        time: obj.time,
+        type: obj.type,
+        x: obj.x,
+        y: obj.y,
+        direction: obj.direction
+    }, obj.data, obj.anim);
+    return n;
 }
