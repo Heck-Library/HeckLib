36e6d5ff2102b143a307e01350aeba7ab06f264f
jevk <jevk@users.noreply.github.com>
2023-04-07 22:01:58 +0300

    Update customEventsToJSON.ts

--- map/converters/customEventsToJSON.ts
+++ map/converters/customEventsToJSON.ts
@@ -13,7 +13,6 @@ export function customEventsToJSON(): Record<string, any>[] {
             let stringified = JSON.stringify(eventJSON);
             if (!V3) {
                 stringified = stringified
-                    .replace('"position":', '"offsetPosition":')
                     .replace('"b":', '"time":')
                     .replace('"t":', '"type":')
                     .replace('"d":', '"data":')
