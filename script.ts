3314ac6754d376018342f3521efb801898442646
jevk <jevk@users.noreply.github.com>
2023-04-24 17:09:26 +0300

    script.ts now has `sortObjects: true` by default

--- script.ts
+++ script.ts
@@ -1,9 +1,5 @@
 import { Difficulty, initialize } from "./map/initialize";
 import { finalize } from "./map/finalize";
-import AnimateTrack from "./events/animateTrack";
-import track from "./functions/track";
-import { notes } from "./map/variables";
-import filter from "./functions/filter";
 
 const INPUT = Difficulty.EXPERT_PLUS_STANDARD;
 const OUTPUT = Difficulty.EXPERT_PLUS_LAWLESS;
@@ -12,12 +8,12 @@ const DIFFICULTY = initialize(INPUT, OUTPUT);
 
 // #region MAP SCRIPT
 
-track(filter(notes, 0, 8), "sdfasdf");
+
 
 
 // #endregion MAP SCRIPT
 
 finalize(DIFFICULTY, {
     formatting: true,
-    
+    sortObjects: true
 });
