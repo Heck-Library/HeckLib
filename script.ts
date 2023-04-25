fb2d1d6ad1a51d12d6c994d53501eebb36f6d3c0
Jevk <58623854+jevk@users.noreply.github.com>
2023-04-25 12:09:10 +0300

    Update script.ts

--- script.ts
+++ script.ts
@@ -1,10 +1,10 @@
 import { Difficulty, initialize } from "./map/initialize";
 import { finalize } from "./map/finalize";
 
-const INPUT = Difficulty.EXPERT_PLUS_STANDARD;
-const OUTPUT = Difficulty.EXPERT_PLUS_LAWLESS;
+const INPUT = Difficulty.EXPERT_PLUS_STANDARD;  // This is your vanilla/input difficulty.
+const OUTPUT = Difficulty.EXPERT_PLUS_LAWLESS;  // This is your modded output difficulty.
 
-const DIFFICULTY = initialize(INPUT, OUTPUT);
+const DIFFICULTY = initialize(INPUT, OUTPUT);   // This initializes the map.
 
 // #region MAP SCRIPT
 
