37f27e33849117b7834787c488b9d3fecd6d47fc
jevk <jevk@users.noreply.github.com>
2023-05-02 02:03:25 +0300

    easing docs

--- consts/easing.ts
+++ consts/easing.ts
@@ -35,10 +35,37 @@ enum InOut {
     Bounce = "easeInOutBounce"
 }
 const Step = "easeStep";
+/**
+ * ### Ease
+ * 
+ * Easing functions specify the rate of change of a parameter over time.
+ * 
+ * @see https://easings.net/ for all easing functions.
+ */
 const ease = {
+    /**
+     * ### easeIn
+     * 
+     * In easings, the motion starts slowly and then accelerates as the motion progresses.
+     */
     In,
+    /**
+     * ### easeOut
+     * 
+     * Out easings, the motion starts quickly and then decelerates to a stop.
+     */
     Out,
+    /**
+     * ### easeInOut
+     * 
+     * InOut easings, the motion starts slowly, accelerates motion, then decelerates to a stop.
+     */
     InOut,
+    /**
+     * ### easeStep
+     * 
+     * Instant transition.
+     */
     Step
 }
 export default ease;
