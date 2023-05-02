6d5cd012d4554eb829d09d86c03c1768c8ebb32e
Jevk <58623854+jevk@users.noreply.github.com>
2023-05-02 12:58:24 +0300

    Update mod.ts

--- effects/mod.ts
+++ effects/mod.ts
@@ -4,11 +4,41 @@ import NoteTrail from "./noteTrail";
 import ParticleTunnel from "./particleTunnel";
 import RandomPath from "./randomPath";
 
+/**
+ * ### Effect
+ * 
+ * `Effect` is a collection of commonly used premade effects that can be applied to the map. These effects are highly customizable, hence they are classes.
+ */
 const Effect = {
+    /**
+     * ### Distortion
+     * 
+     * `Distortion` is a premade effect that distorts the vision in a way where everything looks wavy.
+     */
     Distortion,
+    /**
+     * ### Glitch Distortion
+     * 
+     * `GlitchDistortion` is a premade effect that distorts the vision in a way where everything looks glitchy.
+     */
     GlitchDistortion,
+    /**
+     * ### Note Trail
+     * 
+     * `NoteTrail` is a premade effect that creates a trail of notes behind each note.
+     */
     NoteTrail,
+    /**
+     * ### Particle Tunnel
+     * 
+     * `ParticleTunnel` is a premade effect that creates a tunnel of particles around the player and the track.
+     */
     ParticleTunnel,
+    /**
+     * ### Random Path
+     * 
+     * `RandomPath` is a premade effect that randomizes the path of the notes.
+     */
     RandomPath
 }
 
