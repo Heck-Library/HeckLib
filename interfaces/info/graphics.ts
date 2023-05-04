c9cafc8e121688b95f532f9d59bb6ad8ef6bc34b
jevk <jevk@users.noreply.github.com>
2023-05-04 08:52:51 +0300

    Update graphics.ts

--- interfaces/info/graphics.ts
+++ interfaces/info/graphics.ts
@@ -1,6 +1,6 @@
 export default interface IGraphics {
     mirrorGraphicsSettings?: 0 | 1 | 2 | 3;
-    mainGraphicsSettings?: boolean;
+    mainGraphicsSettings?: 0 | 1;
     smokeGraphicsSettings?: boolean;
     burnMarkTrailsEnabled?: boolean;
     screenDisplacementEffectsEnabled?: boolean;
