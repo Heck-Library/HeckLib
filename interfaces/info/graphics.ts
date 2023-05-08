ce006fcd7a1107fd68e2e540f09e0212edf764d1
jevk <jevk@users.noreply.github.com>
2023-05-08 04:14:35 +0300

    Update graphics.ts

--- interfaces/info/graphics.ts
+++ interfaces/info/graphics.ts
@@ -1,6 +1,24 @@
 export default interface IGraphics {
+    /**
+     * ### Mirror Graphics Settings
+     * 
+     * This is the same as the mirror setting in the game.
+     * 
+     * 0 = Off
+     * 1 = Low
+     * 2 = Medium
+     * 3 = High
+     */
     mirrorGraphicsSettings?: 0 | 1 | 2 | 3;
-    mainGraphicsSettings?: 0 | 1;
+    /**
+     * ### Main Effect Graphics Settings
+     * 
+     * This is the same as the bloom setting in the game.
+     * 
+     * 0 = Off
+     * 1 = On
+     */
+    mainEffectGraphicsSettings?: 0 | 1;
     smokeGraphicsSettings?: boolean;
     burnMarkTrailsEnabled?: boolean;
     screenDisplacementEffectsEnabled?: boolean;
