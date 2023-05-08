be4e30b5c634f053b40d75e2ab65b20e8362c3b6
Jevk <58623854+jevk@users.noreply.github.com>
2023-05-08 13:06:22 +0300

    Settings docs

--- interfaces/info/chroma.ts
+++ interfaces/info/chroma.ts
@@ -1,6 +1,26 @@
 export default interface IChromaSettings {
+    /**
+     * ### Disable Chroma Events
+     * 
+     * Disables Chroma entirely.
+     */
     disableChromaEvents?: boolean;
+    /**
+     * ### Disable Environment Enhancements
+     * 
+     * Disables the environment enhancements that come with Chroma.
+     */
     disableEnvironmentEnhancements?: boolean;
+    /**
+     * ### Disable Note Coloring
+     * 
+     * Disables note coloring.
+     */
     disableNoteColoring?: boolean;
+    /**
+     * ### Force Zen Mode Walls
+     * 
+     * Forces walls on in Zen Mode. Useful if you want to watch a wall map in Zen Mode.
+     */
     forceZenModeWalls?: boolean;
 }
--- interfaces/info/graphics.ts
+++ interfaces/info/graphics.ts
@@ -19,8 +19,28 @@ export default interface IGraphics {
      * 1 = On
      */
     mainEffectGraphicsSettings?: 0 | 1;
+    /**
+     * ### Smoke Graphics Settings
+     * 
+     * This is the same as the smoke setting in the game. Enables particlesystems that emit smoke around the player.
+     */
     smokeGraphicsSettings?: boolean;
+    /**
+     * ### Burn Mark Trails Enabled
+     * 
+     * This enables the burnmarks that appear on the ground when you swing your sabers through it.
+     */
     burnMarkTrailsEnabled?: boolean;
+    /**
+     * ### Screen Displacement Effects Enabled
+     * 
+     * This enables the screen displacement effects, this is the effect that makes walls appear with an opaque shader. This is the same as the screen distortion setting in the game.
+     */
     screenDisplacementEffectsEnabled?: boolean;
+    /**
+     * ### Max Shockwave Particles
+     * 
+     * This is the maximum amount of shockwave particles that can be on screen at once. This is the same as the shockwave particles setting in the game. For maps with walls, it is recommended to be set to 0.
+     */
     maxShockwaveParticles?: 0 | 1 | 2;
 }
