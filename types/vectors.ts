444ac3fe9631769cc850b2ebb232a84e646dd64a
jevk <jevk@users.noreply.github.com>
2023-12-04 21:49:30 +0200

    Update vectors.ts

--- types/vectors.ts
+++ types/vectors.ts
@@ -12,13 +12,14 @@ export type vec3Keyframe =  [number, number, number, number, easingType, typeof 
                             [number, number, number, number, typeof SPLINE, easingType?] |
                             [number, number, number, number];
 export type vec4Keyframe = [number, number, number, number, number, easingType?];
-export type colorKeyframe = [BaseColor, [number, number, number, number, number, colorModifier, easingType?]] |
+export type colorKeyframe = [BaseColor, [number, number, number, number, colorModifier], number, easingType?] |
+                            [BaseColor, number, easingType?] |
                             [number, number, number, number, number, easingType?];
 
 
 export type vec1anim = vec1Keyframe[] | vec1 | string;
 export type vec3anim = vec3Keyframe[] | vec3 | string;
 export type vec4anim = vec4Keyframe[] | vec4 | string;
-export type colorAnim = colorKeyframe[] | vec4 | [BaseColor, [number, number, number, number, colorModifier]] | string;
+export type colorAnim = colorKeyframe[] | vec4 | [BaseColor, [number, number, number, number, colorModifier]] | BaseColor | string;
 
 export type unknownAnim = vec1anim | vec3anim | vec4anim | colorAnim;
