82b6b6ee06d328cb977eb0532e18fad24ae802c5
Jevk <58623854+jevk@users.noreply.github.com>
2025-02-03 11:48:52 +0200

    Update IObjectPrefabData.ts

--- interfaces/events/eventData/IObjectPrefabData.ts
+++ interfaces/events/eventData/IObjectPrefabData.ts
@@ -2,7 +2,7 @@ import { vec3 } from "../../../types/vectors";
 
 export default interface IObjectPrefabData {
     loadMode: "Single" | "Additive";
-    colorNotes: {
+    colorNotes?: {
         track: string | string[];
         asset?: string;
         anyDirectionAsset?: string;
