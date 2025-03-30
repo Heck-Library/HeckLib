3f5ed5b4afe876dfc4344660966051c04862f914
Jevk <58623854+jevk@users.noreply.github.com>
2025-03-30 21:08:22 +0300

    Update randomPath.ts

--- src/util/effect/randomPath.ts
+++ src/util/effect/randomPath.ts
@@ -1,6 +1,43 @@
+import { Ease } from "util/easings";
+import { Random } from "util/functions";
+import { AnyAnimation } from "util/vec";
 import { BaseObject } from "v3/objects/BaseObject";
 
-export function RandomPath(objects: BaseObject) {
+let used = false;
 
+export function RandomPath(objects: BaseObject[], pointDefs: Map<string, AnyAnimation>) {
+    if (!used) {
+        used = true;
+
+        for (let i = 1; i <= 32; i++) {
+            pointDefs.set(`GrpOP${i}`, [
+                [Random(-10, 10), Random(-2, 10), 0, 0],
+                [0, 0, 0, 0.475, Ease.OutCirc]
+            ]);
+            pointDefs.set(`GrpOWR${i}`, [
+                [Random(-45, 45), Random(-45, 45), Random(-45, 45), 0],
+                [0, 0, 0, 0.475, Ease.OutCirc]
+            ]);
+            pointDefs.set(`GrpLR${i}`, [
+                [Random(-45, 45), Random(-45, 45), Random(-180, 180), 0],
+                [0, 0, 0, 0.475, Ease.OutCirc]
+            ]);
+        }
+
+        pointDefs.set("GrpD", [
+            [0, 0],
+            [1, 0.25, Ease.OutSine]
+        ]);
+    }
+
+    objects.forEach((obj) => {
+        if (obj.CustomData.Animation === undefined) obj.CustomData.Animation = {};
+        
+        obj.CustomData.Animation.OffsetPosition = `GrpOP${Random(1, 32)}`;
+        obj.CustomData.Animation.OffsetWorldRotation = `GrpOWR${Random(1, 32)}`;
+        obj.CustomData.Animation.LocalRotation = `GrpLR${Random(1, 32)}`;
+        obj.CustomData.Animation.Dissolve = "GrpD";
+    });
+    
 }
 
