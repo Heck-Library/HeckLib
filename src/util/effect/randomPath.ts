ad0d3b2d15b99d95fefff639520e58fd504f5702
Jevk <58623854+jevk@users.noreply.github.com>
2025-03-31 08:50:41 +0300

    Updated RandomPath.ts

--- src/util/effect/randomPath.ts
+++ src/util/effect/randomPath.ts
@@ -5,8 +5,10 @@ import { BaseObject } from "v3/objects/BaseObject";
 
 let used = false;
 
-export function RandomPath(objects: BaseObject[], pointDefs: Map<string, AnyAnimation>) {
-    if (!used) {
+export function RandomPath(objects: BaseObject[], pointDefs?: Map<string, AnyAnimation>, forcedOffset?: number) {
+    const pointDef = pointDefs !== undefined;
+
+    if (!used && pointDef) {
         used = true;
 
         for (let i = 1; i <= 32; i++) {
@@ -32,11 +34,29 @@ export function RandomPath(objects: BaseObject[], pointDefs: Map<string, AnyAnim
 
     objects.forEach((obj) => {
         if (obj.CustomData.Animation === undefined) obj.CustomData.Animation = {};
+
+        obj.CustomData.Offset = forcedOffset ?? 1;
         
-        obj.CustomData.Animation.OffsetPosition = `GrpOP${Random(1, 32)}`;
-        obj.CustomData.Animation.OffsetWorldRotation = `GrpOWR${Random(1, 32)}`;
-        obj.CustomData.Animation.LocalRotation = `GrpLR${Random(1, 32)}`;
-        obj.CustomData.Animation.Dissolve = "GrpD";
+        obj.CustomData.Animation.OffsetPosition = pointDef ? `GrpOP${Random(1, 32)}` : [
+            [Random(-10, 10), Random(-2, 10), 0, 0],
+            [0, 0, 0, 0.475, Ease.OutCirc]
+        ];
+        obj.CustomData.Animation.OffsetWorldRotation = pointDef ? `GrpOWR${Random(1, 32)}` : [
+            [Random(-45, 45), Random(-45, 45), Random(-45, 45), 0],
+            [0, 0, 0, 0.475, Ease.OutCirc]
+        ];
+        obj.CustomData.Animation.LocalRotation = pointDef ? `GrpLR${Random(1, 32)}` : [
+            [Random(-45, 45), Random(-45, 45), Random(-180, 180), 0],
+            [0, 0, 0, 0.475, Ease.OutCirc]
+        ];
+        obj.CustomData.Animation.Dissolve = pointDef ? "GrpD" : [
+            [0, 0],
+            [1, 0.25, Ease.OutSine]
+        ];
+        obj.CustomData.Animation.DissolveArrow = pointDef ? "GrpD" : [
+            [0, 0],
+            [1, 0.25, Ease.OutSine]
+        ];
     });
     
 }
